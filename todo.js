dc023001457db4f7c1a6a1893fd40aed98dae914
Masamichi Nakamura <masamichi.nakamura@gmail.com>
2019-06-30 22:12:05 +0900

    add watcher

--- todo.js
+++ todo.js
@@ -1,19 +1,36 @@
-var app = new Vue({
+const app = new Vue({
     el: '#app',
     data: {
+        storageKey: 'todolist',
         newtodo: '',
         todolist: []
     },
     methods: {
         addTodo: function () {
-            if (this.newtodo == '') return;
+            if (this.newtodo === '') return;
             this.todolist.push({ text: this.newtodo, done: false, hover: false });
             this.newtodo = '';
         },
         remove: function (index) {
-            if (this.todolist[index].done == true) {
+            if (this.todolist[index].done === true) {
                 this.todolist.splice(index, 1);
             }
         }
     },
+    created: function() {
+        const dataStr = localStorage.getItem(this.storageKey);
+        if (dataStr) {
+            this.todolist = JSON.parse(dataStr);
+        }
+    },
+    watch: {
+        todolist: function() {
+            // 保存
+            localStorage.setItem(
+                this.storageKey,
+                JSON.stringify(this.todolist)
+            );
+        }
+    },
+    
 });
